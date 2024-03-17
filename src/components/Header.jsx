2daa4822dd41442374b443b447b978a3b5ddb12e
shabgard82 <pedramaliyari5@gmail.com>
2024-03-17 18:05:10 +0330

    start project quiz

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="logo512.png" alt="React Logo" />
+      <h1>The React Quiz</h1>
+    </header>
+  );
+}
+export default Header;
