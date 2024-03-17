465a8bdf723b5832913eb9c6ad5f3205502b2d15
shabgard82 <pedramaliyari5@gmail.com>
2024-03-17 19:22:30 +0330

    add header components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
+  useEffect(function () {
+    async function fetchData() {
+      const res = await fetch("http://localhost:9000/Questions");
+      const data = await res.json();
+      console.log(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div className="app">
       <Header />
